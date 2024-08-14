d04be9eeb4869754e3f69cec9dc84ca5efda678a
kavinl <kavinlam327@gmail.com>
2024-08-13 22:15:57 -0600

    webbuilder built

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 JavaScript
-function updateOutput() {
-  const name = document.getElementById("name").value;
-  const message = document.getElementById("message").value;
 
-  const outputDiv = document.getElementById("output");
-  outputDiv.innerHTML = `
-    <h2>Hello, ${name}!</h2>
-    <p>${message}</p>
-  `;
-}
+function updateOutput() {
+    const title = document.getElementById("title").value.replace(/\s/g, '%20');
+    const subtitle = document.getElementById("subtitle").value.replace(/\s/g, '%20');
+    const body = document.getElementById("body").value.replace(/\s/g, '%20');
+    const link = document.getElementById("link").value.replace(/\s/g, '%20');
+    const imagelink = document.getElementById("image-link").value;
+  
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+      <h2>https://mobile-web-hazel.vercel.app/?title=${title}&subititle=${subtitle}&body=${body}&link=%20${link}&image=${imagelink}</p>
+
+    
+    `;
+  }
